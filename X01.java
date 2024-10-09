325562bd259fd67f1c53f4788fcd58f9f987d728
Maxwell Rumahorbo <maxrumbo06@gmail.com>
2024-10-09 16:28:41 -0700

    UPDATE

--- X01.java
+++ X01.java
@@ -4,7 +4,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class Q01 {
+public class X01 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
