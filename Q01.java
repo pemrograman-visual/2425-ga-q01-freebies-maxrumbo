8e51ea97e36af67a3fa282bb4f8209cd156ec101
Maxwell Rumahorbo <maxrumbo06@gmail.com>
2024-10-09 15:54:58 -0700

    PAIR 20

---
+++ Q01.java
@@ -0,0 +1,41 @@
+// 12S24037 - Maxwell Rumahorbo
+// 12S24030 - Swasti Sihombing
+
+import java.util.*;
+import java.lang.Math;
+
+public class Q01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n;
+        double minimum, jumlahBuku, transaksi;
+        double[] hargaBuku = new double[100];
+
+        minimum = 0;
+        n = 0;
+        jumlahBuku = 0;
+        hargaBuku[n] = Double.parseDouble(input.nextLine());
+        minimum = hargaBuku[n];
+        while (hargaBuku[n] != 0) {
+            jumlahBuku = jumlahBuku + hargaBuku[n];
+            if (minimum < hargaBuku[n]) {
+                minimum = minimum;
+            } else {
+                minimum = hargaBuku[n];
+            }
+            n = n + 1;
+            hargaBuku[n] = Double.parseDouble(input.nextLine());
+        }
+        if (jumlahBuku >= 100.0) {
+            transaksi = jumlahBuku - minimum;
+        } else {
+            transaksi = jumlahBuku;
+        }
+        System.out.println(toFixed(transaksi,2));
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
